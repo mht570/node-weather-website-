394c8e3abb4510df090482ae0e0c04570d77aec6
mht570 <kumar.mohit570@gmail.com>
2019-10-12 16:29:18 +0530

    app.js changed by removing process

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-const port=process.env.PORT || 3000
 
 messageOne.textContent=''
 
